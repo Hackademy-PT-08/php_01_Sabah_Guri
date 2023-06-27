2d3ebc61d2791934305a7fc4c3d6024bf027085e
SabahGuri <guri.sabah@icloud.com>
2023-06-27 19:05:39 +0200

    traccia 1

---
+++ selfwork1.php
@@ -0,0 +1,14 @@
+<?php
+
+// traccia 1 
+
+ $interger=3; //numero intero
+ $float=2.2; // numero con decimali
+ $string="ciao"; // stringa
+ $boolean=true; // booleano
+
+ const INTEGER=3;
+ const FLOAT=2.2;
+ const STRING="ciao";
+ const BOOLEAN=true;
+?>
